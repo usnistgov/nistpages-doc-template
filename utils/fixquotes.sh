768f8e8922333435fa282422dee6021b6320d216
Justin Richer <justin@bspk.io>
2024-06-28 15:11:06 -0400

    updated css, added quote scanner

---
+++ utils/fixquotes.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# GNU All-Permissive License
+SED=$(which sed)
+SDQUO=$(echo -ne '\u2018\u2019')
+RDQUO=$(echo -ne '\u201C\u201D')
+$SED -i -e "s/[$SDQUO]/\'/g" -e "s/[$RDQUO]/\"/g" "${1}"
